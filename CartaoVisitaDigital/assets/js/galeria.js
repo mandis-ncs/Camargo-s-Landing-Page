820797445c53c8a974bbc29dc9f017a39ba76c7d
mandis-ncs <amnd.castro@gmail.com>
2023-11-03 13:09:45 -0300

    isotope not working

--- CartaoVisitaDigital/assets/js/galeria.js
+++ CartaoVisitaDigital/assets/js/galeria.js
@@ -23,40 +23,21 @@
       });
 
 
-/*LIGHTBOX JS*/
-lightgallery
 
   
    /**
    * Init isotope layout and filters
    */
-  function initIsotopeLayout() {
-    document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
-      let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
-      let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
-      let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
-
-      let initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
-        itemSelector: '.isotope-item',
-        layoutMode: layout,
-        filter: filter,
-        sortBy: sort
-      });
-
-      isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
-        filters.addEventListener('click', function() {
-          isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
-          this.classList.add('filter-active');
-          initIsotope.arrange({
-            filter: this.getAttribute('data-filter')
-          });
-          if (typeof aosInit === 'function') {
-            aosInit();
-          }
-        }, false);
-      });
-
+   $(document).ready(function() {
+    // Initialize Isotope
+    var $listing = $('.gallery').isotope({
+      itemSelector: '.isotope-item',
+      layoutMode: 'fitRows'
     });
-  }
-  window.addEventListener('load', initIsotopeLayout);
 
+    // Filter button click event
+    $("#filters").on("click", "button", function() {
+      var filterValue = $(this).attr('data-filter');
+      $listing.isotope({ filter: filterValue });
+    });
+  });
